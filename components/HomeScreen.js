c5582d624389a110c2af8ed60e59987f1139735c
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-04-06 23:42:41 -0300

    Started addign list to shop on home screen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default class HomeScreen extends React.Component {
+
+
+class ShopList extends React.Component {
     constructor(props) {
         super(props)
+
+        this.shopList = Array(5).fill({'date': '23/03/2019', 'title': 'nome do estabelecimento'})
     }
+
+    render(){
+        return(
+            <FlatList
+                data={ this.shopList }
+                renderItem={ ({item}) => (
+                    <Text>{item.title} - {item.date}</Text>
+                )}
+            />
+        )
+    }
+}
+
+
+export default class HomeScreen extends React.Component {
 
     static navigationOptions = {
         title: 'Minhas Compras'
@@ -17,6 +36,8 @@ export default class HomeScreen extends React.Component {
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
                 <Text>Aqui vai a listagem de compras anteriores</Text>
+
+                <ShopList />
 
                 <ActionButton buttonColor="rgba(231,76,60,1)" onPress= { () => this.props.navigation.push("NewShopScreen")} >
                 </ActionButton>
