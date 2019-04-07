01d6fed02d275c1e786a5f061e7698ac7a495dd3
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-04-07 00:37:55 -0300

    Added react-native-elements package (used to list shopping in home screen)

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements'
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -8,16 +9,20 @@ import Icon from 'react-native-vector-icons/Ionicons';
 class ShopList extends React.Component {
     constructor(props) {
         super(props)
-
-        this.shopList = Array(5).fill({'date': '23/03/2019', 'title': 'nome do estabelecimento'})
+        this.shopList = Array(1).fill({ 'date': '23/03/2019', 'title': 'nome do estabelecimento' })
     }
 
-    render(){
-        return(
+    render() {
+        console.log(this.shopList)
+        return (
+
             <FlatList
-                data={ this.shopList }
-                renderItem={ ({item}) => (
-                    <Text>{item.title} - {item.date}</Text>
+                data={this.shopList}
+                renderItem={({ item }) => (
+                    <ListItem
+                        title={item.title}
+                        subtitle={item.date}
+                    />
                 )}
             />
         )
@@ -35,11 +40,13 @@ export default class HomeScreen extends React.Component {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-                <Text>Aqui vai a listagem de compras anteriores</Text>
+                <Text>Compras anteriores</Text>
 
-                <ShopList />
+                <View style={styles.scene}>
+                    <ShopList />
+                </View>
 
-                <ActionButton buttonColor="rgba(231,76,60,1)" onPress= { () => this.props.navigation.push("NewShopScreen")} >
+                <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => this.props.navigation.push("NewShopScreen")} >
                 </ActionButton>
             </View>
         );
@@ -48,6 +55,11 @@ export default class HomeScreen extends React.Component {
 
 
 const styles = StyleSheet.create({
+    scene: {
+        flex: 1,
+        paddingTop: 25,
+        width: '100%'
+    },
     actionButtonIcon: {
         fontSize: 20,
         height: 22,
