0860673f00e0395c426853bcc55385d193b37384
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-04-07 13:23:46 -0300

    Update HomeScreen.js
    Changed the way the ShopList is rendered (HomeScreen class now has an array of data and each element is used to create a ShopList object that renders the data)
    Added some styling to shoplist
    Removed unsed import

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,28 +2,23 @@ import React from 'react';
 import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements'
 import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
-
 
 
 class ShopList extends React.Component {
     constructor(props) {
         super(props)
-        this.shopList = Array(1).fill({ 'date': '23/03/2019', 'title': 'nome do estabelecimento' })
+
+        this.state = {
+            title: props.value.title,
+            date: props.value.date
+        }
     }
 
     render() {
-        console.log(this.shopList)
         return (
-
-            <FlatList
-                data={this.shopList}
-                renderItem={({ item }) => (
-                    <ListItem
-                        title={item.title}
-                        subtitle={item.date}
-                    />
-                )}
+            <ListItem containerStyle={styles.listitem}
+                title={this.state.title}
+                subtitle={this.state.date}
             />
         )
     }
@@ -31,9 +26,20 @@ class ShopList extends React.Component {
 
 
 export default class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.shopList = Array(3).fill({ 'date': '23/03/2019', 'title': 'nome do estabelecimento' })
+    }
 
     static navigationOptions = {
         title: 'Minhas Compras'
+    }
+
+    renderShopList(item, index) {
+        return (
+            <ShopList value={item} key={index} />
+        )
     }
 
     render() {
@@ -43,7 +49,7 @@ export default class HomeScreen extends React.Component {
                 <Text>Compras anteriores</Text>
 
                 <View style={styles.scene}>
-                    <ShopList />
+                    {this.shopList.map(this.renderShopList)}
                 </View>
 
                 <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => this.props.navigation.push("NewShopScreen")} >
@@ -59,6 +65,11 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 25,
         width: '100%'
+    },
+    listitem: {
+        backgroundColor: 'red',
+        marginTop: 20,
+
     },
     actionButtonIcon: {
         fontSize: 20,
